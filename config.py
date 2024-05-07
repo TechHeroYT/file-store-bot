51d217430bf79c7db3e73435c205329ccf542b8e
Tech HEro <163621970+TechHeroYT@users.noreply.github.com>
2024-05-07 15:30:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -81,9 +81,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://file-store-bot-part1-hero.onrender.com"
+        URL = "https://file-store-bot-0u3i.onrender.com"
     else:
-        URL = "https://file-store-bot-part1-hero.onrender.com"
+        URL = "https://file-store-bot-0u3i.onrender.com"
 
 
 
